d58f6beaf5a797983493590473167c5446d1db15
Andrea MEAZZA <ameazza@it.iliad.com>
2024-07-14 21:48:48 +0200

    fix: some issue

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
-# database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://meahome_postgres_user:SXkSicAdQfG40H06i5bkwVb3pHWg37kp@dpg-cq9vj1lds78s739k1u10-a/meahome_postgres"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://meahome_postgres_user:SXkSicAdQfG40H06i5bkwVb3pHWg37kp@dpg-cq9vj1lds78s739k1u10-a/meahome_postgres")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
