9af825c331ed268a8e46a4b6f663b19acbddcc0b
Andrea MEAZZA <ameazza@it.iliad.com>
2024-07-14 20:22:09 +0200

    fix: db ul

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://ameazza:password@db:5432/nudges"
+DATABASE_URL = "postgresql://meahome_postgres_user:SXkSicAdQfG40H06i5bkwVb3pHWg37kp@dpg-cq9vj1lds78s739k1u10-a/meahome_postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
